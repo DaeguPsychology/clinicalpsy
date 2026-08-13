a06ce6cfe4f9f922d0719e7c10e3d524cb0c765a
daegupsy <neets11@gmail.com>
2026-08-13 21:54:15 +0900

    Add files via upload

--- layout.js
+++ layout.js
@@ -44,7 +44,7 @@
 '      <span class="b-text"><strong>임상심리연구소</strong></span>' +
 '    </a>' +
 '    <nav class="gnb">\n      ' + gnb + '\n    </nav>' +
-'    <button class="burger" aria-label="메뉴 열기/닫기" onclick="document.querySelector(\'header\').classList.toggle(\'nav-open\')">&#9776;</button>' +
+'    <button class="burger" aria-label="메뉴 열기/닫기" onclick="document.querySelector(\'.gnb\').classList.toggle(\'open\')">&#9776;</button>' +
 '  </div>' +
 '</header>';
 
